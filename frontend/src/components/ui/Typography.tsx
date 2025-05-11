74d5ea9b2774dda3950e9aa6b7543579248f5b0b
Nasrul Huda <nasrul2001@gmail.com>
2025-05-11 14:14:08 +0200

    Added sidebar

--- frontend/src/components/ui/Typography.tsx
+++ frontend/src/components/ui/Typography.tsx
@@ -3,11 +3,22 @@ interface TypographyH2Props {
     title: string;
 }
 
+interface TypographyH3Props {
+    title: string;
+}
 
 export function TypographyH2({ title }: TypographyH2Props) {
     return (
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         {title}
       </h2>
+    )
+  }
+
+export function TypographyH3({ title }: TypographyH3Props) {
+    return (
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        {title}
+      </h3>
     )
   }
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import { getUserData, logout } from '../services/auth/github';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { useNavigate } from 'react-router-dom';
+import { SidebarGroup, SidebarGroupLabel, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Sidebar, SidebarHeader, SidebarContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from '@/components/ui/sidebar';
-import { TypographyH2 as Typography } from '@/components/ui/TypographyH2';
+import { TypographyH2, TypographyH3 } from '@/components/ui/Typography';
+import { Button } from '@/components/ui/button';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
 
 interface User {
   id: number;
@@ -13,7 +16,7 @@ interface User {
 
 const Dashboard= () => {
   const [user, setUser] = useState<User | null>(null);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const userData = getUserData();
     if (userData) {
@@ -25,22 +28,39 @@ const Dashboard= () => {
     return <div>Loading...</div>;
   }
 
+  const handleGoback = () => {
+    navigate(-1);
+  }
+
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
   return (
     <SidebarProvider>
       <Sidebar>
         <SidebarHeader>
-          <Typography title="GitSum" />
+          <TypographyH2 title="GitSum" />
         </SidebarHeader>
         <SidebarContent>
           <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton>Home</SidebarMenuButton>
-            </SidebarMenuItem>
+            <SidebarGroup>
+              <SidebarGroupLabel className="flex items-center justify-between">
+                <TypographyH3 title="Repositories" />
+                <Button variant="default" size="sm" onClick={handleGoback}>
+                  <SidebarTrigger>
+                    <ArrowLeftIcon className="w-4 h-4" />
+                  </SidebarTrigger>
+                </Button>
+              </SidebarGroupLabel>
+            </SidebarGroup>
           </SidebarMenu>
         </SidebarContent>
       </Sidebar>
       <main>
-        <SidebarTrigger />
+        <SidebarTrigger className="absolute top-2 left-2 text-white" />
       </main>
     </SidebarProvider>
   );
